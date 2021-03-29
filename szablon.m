8c01530989152d0e9084a528076f995cbf945256
Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>
2021-03-29 12:40:46 +0200

    Signed-off-by: Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>

---
+++ szablon.m
@@ -0,0 +1,9 @@
+% Przedmiot: Techniki Obliczeniowe 
+% Kierunek studiów: Mechatronika 
+% Semestr: 2
+% Rok akademicki: 2020/2021
+% Data (dzień-miesiąc-rok): <<dd-mm-2021>>
+%
+% Imię:             <<imię>>
+% Nazwisko:         <<nazwisko>>
+% Numer albumu ZUT: <<numer albumu>>
