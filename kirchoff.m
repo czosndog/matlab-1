80e96464489c92a34d0571f77030bb3b4eb4b4f7
Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>
2021-04-12 16:06:48 +0200

    Signed-off-by: Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>

--- kirchoff.m
+++ kirchoff.m
@@ -6,3 +6,29 @@ V2 = 1.5;
 R1 = 1000; % oporności - wartość w omach
 R2 = 1000;
 R3 = 1000;
+
+A = [
+    -1  0 -1 +1  0
+     0 -1 +1  0 -1
+     R1 0  0  0  0
+     0  R2 0  0  0
+     0  0  R3 0  0
+    ];
+
+b = [
+    0
+    0
+    V1
+    V2
+    (V1-V2)
+    ];
+
+disp('natężenia prądów [w amperach]');
+
+I = A \ b
+
+if any(abs(I) >= Imax)
+    disp('natężenie prądu przekroczone');
+else
+    disp('natężenie prądu w dopuszczalnym zakresie');
+end
